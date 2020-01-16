20a0c5db8ea21a77670ba9e031e0d1a9f241ab73
Felipe S Yopan <felipesiedsyopan@gmail.com>
2020-01-15 22:35:13 -0200

    Logica feita para 0-999, com excessao de 11-19

--- logica.py
+++ logica.py
@@ -1,2 +1,52 @@
-entrada = input('Informe a entrada: ')
-print(entrada)
+unidades = ('zero','um','dois','tres','quatro',
+	'cinco', 'seis', 'sete', 'oito', 'nove')
+
+xi_xix = ('onze', 'doze', 'treze', 'quatorze', 'quinze',
+	'dezesseis', 'dezessete', 'dezoito', 'dezenove')
+
+dezenas = ('', 'dez','vinte','trinta','quarenta','cinquenta',
+	'sessenta', 'setenta', 'oitenta', 'noventa')
+
+centenas = ('', 'cento','duzentos','trezentos','quatrocentos','quinhentos',
+	'seiscentos', 'setecentos', 'oitocentos', 'novecentos')
+
+
+def tradutor(entrada):
+	tamanho = len(entrada)
+	neg = entrada.find('-')
+	if neg == -1:
+		print('Não é negativo')
+		negativo = 0
+	else:
+		tamanho = tamanho-1
+		negativo = 1
+
+	if tamanho == 1:
+		saida = unidades[int(entrada)]
+
+	elif tamanho == 2:
+		if entrada == '10':
+			saida = dezenas[1]
+		elif entrada == '20':
+			saida = dezenas[2]
+		elif entrada[1] == '0':
+			saida = dezenas[int(entrada[0])]
+		else:
+			saida = dezenas[int(entrada[0])] + ' e ' + unidades[int(entrada[1])]
+
+	elif tamanho == 3:
+		if entrada == '100':
+			saida = 'cem'
+		elif entrada[1] == '0':
+			saida = centenas[int(entrada[0])] + ' e ' + unidades[int(entrada[2])]
+
+		elif entrada[2] == '0':
+			saida = centenas[int(entrada[0])] + ' e ' + dezenas[int(entrada[1])]
+		else:
+			saida = centenas[int(entrada[0])] + ' e ' + dezenas[int(entrada[1])] + ' e ' + unidades[int(entrada[2])]
+
+	return saida
+
+while True:
+	entrada = input('Informe um numero de 0 a 99: ')
+	print(tradutor(entrada))
