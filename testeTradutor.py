bb452cc67d26de6d4d0ae364f1afbe0acb15acdd
Felipe S Yopan <felipesiedsyopan@gmail.com>
2020-01-16 17:45:49 -0200

    Versao com arquivo de resultado reduzido, para poder ler no github

--- testeTradutor.py
+++ testeTradutor.py
@@ -2,7 +2,7 @@ import logica
 
 arquivo = open('provaReal.txt', 'w')
 
-for i in range(-99999,100000):
+for i in range(-9999,10000):
 	letra = str(i)
 	linha = letra + ' -> ' + logica.tradutor(letra) + '\n'
 	arquivo.write(linha)
